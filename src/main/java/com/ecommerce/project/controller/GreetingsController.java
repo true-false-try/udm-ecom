235e78335ff5d0cc38e7cb7fc22ef5622ad6a760
true-false-try <6mateo6ch@gmail.com>
2025-01-29 17:17:55 +0200

    [SECT_14] - added signin endpoint with request and response bodies

--- src/main/java/com/ecommerce/project/controller/GreetingsController.java
+++ src/main/java/com/ecommerce/project/controller/GreetingsController.java
@@ -1,11 +1,37 @@
 package com.ecommerce.project.controller;
 
+import com.ecommerce.project.jwt.JwtUtils;
+import com.ecommerce.project.jwt.LoginRequest;
+import com.ecommerce.project.jwt.LoginResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static java.util.stream.Collectors.toList;
 
 @RestController
+@RequiredArgsConstructor
 public class GreetingsController {
+    private final JwtUtils jwtUtils;
+    private final AuthenticationManager authenticationManager;
 
     @GetMapping("/hello")
     public String sayHello() {
@@ -22,5 +48,28 @@ public class GreetingsController {
     @PreAuthorize("hasRole('ADMIN')")
     public String  adminEndpoint() {
         return "Hello,  ADMIN";
+    }
+
+    @PostMapping("signin")
+    public ResponseEntity<Object> authenticateUser(@RequestBody LoginRequest loginRequest) {
+        Authentication authentication;
+        try {
+            authentication = authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getUsername(), loginRequest.getPassword()));
+        } catch (AuthenticationException ex) {
+            Map<String, Object> map = new HashMap<>();
+            map.put("message", "Bad credentials");
+            map.put("status", false);
+            return new ResponseEntity<>(map, HttpStatus.NOT_FOUND);
+        }
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        String jwtToken = jwtUtils.generateTokenFromUsername(userDetails);
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority).toList();
+        LoginResponse loginResponse = new LoginResponse(userDetails.getUsername(), userDetails.getPassword(), roles);
+        return ResponseEntity.ok(loginResponse);
+
+
     }
 }
