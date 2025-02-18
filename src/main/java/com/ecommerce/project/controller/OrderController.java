89667e6ebb6faaade6a6ab6f4ff6b71deff3f944
true-false-try <6mateo6ch@gmail.com>
2025-02-18 18:38:54 +0200

    [SECT_20] - added controller

---
+++ src/main/java/com/ecommerce/project/controller/OrderController.java
@@ -0,0 +1,35 @@
+package com.ecommerce.project.controller;
+
+import com.ecommerce.project.payload.OrderDto;
+import com.ecommerce.project.payload.OrderRequestDto;
+import com.ecommerce.project.util.AuthUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class OrderController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @Autowired
+    private AuthUtil authUtil;
+
+    @PostMapping("/order/users/payments/{paymentMethod}")
+    public ResponseEntity<OrderDto> orderProducts(@PathVariable String paymentMethod, @RequestBody OrderRequestDto orderRequestDTO) {
+        String emailId = authUtil.loggedInEmail();
+        OrderDto order = orderService.placeOrder(
+                emailId,
+                orderRequestDTO.getAddressId(),
+                paymentMethod,
+                orderRequestDTO.getPgName(),
+                orderRequestDTO.getPgPaymentId(),
+                orderRequestDTO.getPgStatus(),
+                orderRequestDTO.getPgResponseMessage()
+        );
+        return new ResponseEntity<>(order, HttpStatus.CREATED);
+    }
+}
