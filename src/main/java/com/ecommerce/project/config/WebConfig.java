ffabf360583641e2ccd281df2dd2df1c74f5d910
true-false-try <6mateo6ch@gmail.com>
2025-04-18 22:23:12 +0300

    [SECT_38] - fixed problem wit CORS at frontend

---
+++ src/main/java/com/ecommerce/project/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.ecommerce.project.config;
+
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${frontend.url}")
+    private String frontendUrl;
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://lo calhost:3000", frontendUrl)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
